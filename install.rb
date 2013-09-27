5dcd819ee8e331ff8cbe1153c881bbb283c92ecf
Isaac Sloan <isaac@isaacsloan.com>
2013-09-27 16:32:46 -0600

    everything is better

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
-dotfilepath = File.expand_path(File.dirname(__FILE__))
+dotfilepath = File.join(File.expand_path(File.dirname(__FILE__)), "dot")
 simlinkpath = Dir.pwd
-dotfilelist = Dir.entries(dotfilepath).select{|f| !%w(. .. .DS_Store .git .gitignore).include?(f)}.select{|f| !(f =~ /^\.\w/).nil?}
+dotfilelist = Dir.entries(dotfilepath).select{|f| !%w(. ..).include?(f)}
 backupdir = simlinkpath+"/dotfilebackup_"+Time.now.strftime("%Y%m%d%H%M%S")
 Dir.mkdir(backupdir)
-dotfilelist.each{|f|  `mv #{simlinkpath}/#{f} #{backupdir}/`}
-dotfilelist.each{|f| `ln -fs #{dotfilepath}/#{f} #{simlinkpath}`}
-`cp #{dotfilepath}/fonts/* ~/Library/Fonts/`
+dotfilelist.each{|f|  `mv #{simlinkpath}/.#{f} #{backupdir}/`}
+dotfilelist.each{|f| `ln -fs #{dotfilepath}/#{f} #{simlinkpath}/.#{f}`}
+`cp #{dotfilepath}/../fonts/* ~/Library/Fonts/`
